df135b504c8be6b6de0fc492edd8f9ea1a13398b
dei0811 <deinergonzalezparedes12@gmail.com>
2026-01-23 15:52:51 -0500

    Comment out React and component imports
    Commented out the import statements for React and components.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+/*import React, { useState, useEffect } from 'react';
 import ImageCard from './components/ImageCard';
 import TourModal from './components/TourModal';
 import { TourPoint } from './types';
